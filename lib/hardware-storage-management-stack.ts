31ac1690f70fc18c3e6529e60a701e8ef373d75d
ChauBQN <nhatchau.bq@gmail.com>
2024-04-09 13:43:20 +0700

    update

--- lib/hardware-storage-management-stack.ts
+++ lib/hardware-storage-management-stack.ts
@@ -1,32 +1,26 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
-import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
+import { NodejsFunction } from './lambda';
 
 export class HardwareStorageManagementStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const helloFunction = new NodejsFunction(this, 'HelloFunction', {
-      entry: 'lambdas/hello.ts',
-    });
-    const welcomeFunction = new NodejsFunction(this, 'WelcomeFunction', {
-      entry: 'lambdas/welcome.ts',
-    });
-    const getAllTodosFunction = this.createNodeJsFunction(
+    const helloFunction = NodejsFunction(
+      this,
+      'HelloFunction',
+      'lambdas/hello.ts'
+    );
+    const welcomeFunction = NodejsFunction(
+      this,
+      'WelcomeFunction',
+      'lambdas/welcome.ts'
+    );
+    const getAllTodosFunction = NodejsFunction(
+      this,
       'GetAllTodosFunction',
       'lambdas/todos.get.ts'
     );
-  }
-
-  createNodeJsFunction(
-    functionName: string,
-    entry: string,
-    runtime: Runtime = Runtime.NODEJS_20_X
-  ): NodejsFunction {
-    return new NodejsFunction(this, functionName, {
-      runtime: runtime,
-      entry,
-    });
   }
 }
